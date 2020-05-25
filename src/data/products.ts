e30b3766ec0fda05767ef186bb669f55b9ce4c25
Thomas Tharaud <ttharaud@openwt.com>
2020-05-25 18:45:45 +0200

    image fixed for Red Epic-W

--- src/data/products.ts
+++ src/data/products.ts
@@ -37,19 +37,19 @@ export const ProductsData: Product[] = [
     new Product(420, ['#2f3640'], 'Applewatch series 5', 11, ['40mm', '44mm'], 'applewatch.jpg'),
     new Product(280, ['#2f3542', '#0652DD'], 'Galaxy watch', 11, [], 'galaxywatch.jpg'),
     new Product(169, ['#2f3542', '#a4b0be', '#f8c291', '#dfe6e9'], 'Watch GT 2', 11, [], 'watchgt.jpg'),
-    
-    new Product(45000, ['#2d3436', '#d63031'], 'Red Epic-W', 13, [], 'RED.png'),
+
+    new Product(45000, ['#2d3436', '#d63031'], 'Red Epic-W', 13, [], 'red.png'),
     new Product(349, ['#2d3436'], 'DJI osmo pocket', 13, [], 'osmopocket.jpg'),
     new Product(329, ['#2d3436'], 'Go Pro Hero 8', 13, [], 'gopro8.jpg'),
-    
+
     new Product(490, ['#2f3542'], 'Canon EOS 2000D', 14, [], 'canon1.png'),
     new Product(3580, ['#2d3436'], 'Nikon D850', 14, [], 'nikon.jpg'),
-    
+
     new Product(180, ['#f5f6fa', '#2f3640'], 'Logitech G935', 16, [], 'g935.jpeg'),
     new Product(179, ['#dfe4ea'], 'Airpods', 16, [], 'airpods.png'),
     new Product(279, ['#dfe4ea'], 'Airpods Pro', 16, [], 'airpodspro.jpg'),
-    
+
     new Product(175, ['#2f3542'], 'G560', 17, [], 'g560.png'),
     new Product(199, ['#2f3640'], 'Yamaha HS7', 17, ['28x21x33cm'], 'hs7.jpg'),
-    
+
 ];
