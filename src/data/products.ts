c13e79f84a4f11b7c79e736261cdff13a911cf47
Withomas <other.thomass@gmail.com>
2020-06-09 12:51:49 +0200

    Feature/add last modification 2 (#8)
    * ajout du composant cards
    
    * derniere-versions
    
    * merge conflicts fixed
    
    * remove unused files
    
    * fix product file
    
    * clean file
    
    Co-authored-by: kil23levrai <thimeo.delaunay@laposte.net>
    Co-authored-by: Thomas Tharaud <ttharaud@openwt.com>

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,13 +1,10 @@
 import { Product } from 'src/app/entities/product';
-import { getQueryPredicate } from '@angular/compiler/src/render3/view/util';
-import { getCurrencySymbol } from '@angular/common';
 
 export const ProductsData: Product[] = [
     new Product(1159, ['#57606f', '#d1ccc0', '#218c74', '#ffda79'], 'IPhone 11 pro', 2, ['6,1"'], '11pro.png'),
     new Product(1850, ['#d1ccc0', '#1e272e'], 'Samsung Fold', 2, ['7,3"'], 'fold.png'),
     new Product(15000, ['#485460', '#d2dae2'], 'LG signature TV R9', 3, ['88"', '79"'], 'lgsignature.jpg'),
     new Product(379, ['#1e272e'], 'Philips 43PUS6704', 3, ['43"', '52"'], 'TVphilips.jpg'),
-
     new Product(420, ['#2f3640'], 'Applewatch series 5', 11, ['40mm', '44mm'], 'applewatch.png'),
     new Product(850, ['#353b48'], 'Samsung LC49HG90DMU', 7, ['49"'], 'ecran.jpg'),
     new Product(300, ['#1e272e'], 'Logitech G29', 8, [], 'g29.jpg'),
@@ -16,6 +13,5 @@ export const ProductsData: Product[] = [
     new Product(45000, ['#2d3436', '#d63031'], 'Red Epic-W', 10, [], 'red.png'),
     new Product(4500, ['#273c75', '#95a5a6'], 'Rolex Submariner', 11, ['40mm'], 'rolex.png'),
     new Product(120, ['#2d3436', '#d63031'], 'Nike tn', 10, ['35', '38', '40', '46'], 'tn.jpg'),
-    new Product(1800, ['#2d3436'], 'MSI Aegis ti3', 6, [], 'tourpc.jpg'),
-
+    new Product(1800, ['#2d3436'], 'MSI Aegis ti3', 6, [], 'tourpc.jpg')
 ];
